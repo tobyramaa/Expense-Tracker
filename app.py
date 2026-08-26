999f35852a4f92dc0b338562a0a669a11c9d8fdf
Tobiloba Oyeniyi <oyeniyitobiloba12@gmail.com>
2026-08-26 10:53:29 +0100

    Added the balance section on my dashboard

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ login_manager.login_message = "Please login first."
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 app.register_blueprint(auth)
 app.register_blueprint(main)
